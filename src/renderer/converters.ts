6eb9a2c73c95a575dbf56300b8e66c6d7d611b87
Kamil Rybacki <kamilandrzejrybacki@gmail.com>
2024-07-29 19:25:29 +0200

    start converters tests

--- src/renderer/converters.ts
+++ src/renderer/converters.ts
@@ -3,19 +3,45 @@ import { html } from 'satori-html';
 import { JSDOM } from 'jsdom';
 import { SatoriFontInfo } from '@renderer/fonts';
 
-export const markupToSvg = async (
-  markup: string,
-  size: { width: number; height: number },
-  fonts: SatoriFontInfo[]
-): Promise<SVGElement> => {
-  return satori(html`${markup}`, {
-    width: size.width,
-    height: size.height,
-    fonts: fonts as SatoriFontInfo[],
-  })
-    .then((svgSource: string) => {
-      return new JSDOM(svgSource)
-        .window.document
-        .querySelector('svg') as SVGElement;
+export namespace Markup {
+  export const toSVG = async (
+    markup: string,
+    size: { width: number; height: number },
+    fonts: SatoriFontInfo[]
+  ): Promise<SVGElement> => {
+    return satori(html`${markup}`, {
+      width: size.width,
+      height: size.height,
+      fonts: fonts as SatoriFontInfo[],
+    })
+      .then((svgSource: string) => {
+        return new JSDOM(svgSource)
+          .window.document
+          .querySelector('svg') as SVGElement;
+      });
+  }
+};
+
+export namespace SVG {
+  export const toDataURL = (svg: SVGElement): string => {
+    return `data:image/svg+xml,${encodeURIComponent(svg.outerHTML)}`;
+  }
+
+  export const toPNG = async (svg: SVGElement): Promise<string> => {
+    const canvas = new JSDOM().window.document.createElement('canvas');
+    const context = canvas.getContext('2d');
+
+    canvas.width = parseInt(svg.getAttribute('width') || '0');
+    canvas.height = parseInt(svg.getAttribute('height') || '0');
+
+    const image = new Image();
+    image.src = toDataURL(svg);
+
+    return new Promise((resolve) => {
+      image.onload = () => {
+        context?.drawImage(image, 0, 0);
+        resolve(canvas.toDataURL('image/png'));
+      }
     });
+  }
 };
--- tests/renderer/converters.test.ts
+++ tests/renderer/converters.test.ts
@@ -1,16 +1,18 @@
-import { markupToSvg } from "@renderer/converters";
+import { Markup, SVG } from "@renderer/converters";
 import { describe, it, expect } from '@jest/globals';
 import { GoogleFonts, SatoriFontInfo } from "@renderer/fonts";
+import { JSDOM } from 'jsdom';
+import * as fs from 'fs';
 
 const TEST_MARKUP_CONTENT = '<h1>Hello, World!</h1>';
 const TEST_SVG_DIMENSIONS = { width: 100, height: 100 };
 
-describe('Markup to SVG converter', () => {
+describe('Markup converters', () => {
     it('should be able to convert HTML markup to SVG', async () => {
       const renderedSVG: SVGElement = await GoogleFonts
         .getDefaultFont()
         .then(async (fetchedFont: SatoriFontInfo) => {
-          return await markupToSvg(
+          return await Markup.toSVG(
             TEST_MARKUP_CONTENT,
             TEST_SVG_DIMENSIONS,
             [fetchedFont]
@@ -28,3 +30,26 @@ describe('Markup to SVG converter', () => {
       });
     });
 });
+
+// describe('SVG converters', () => {
+//   const TEST_SVG = fs.readFileSync('tests/assets/test.svg', 'utf-8');
+//   const DOM = new JSDOM(TEST_SVG);
+
+//   it('should be able to convert to data URL', () => {
+//     const svgElement = DOM.window.document.querySelector('svg') as SVGElement;
+    
+//     const svgDimensions = {
+//       width: parseInt(svgElement.getAttribute('width') || '0'),
+//       height: parseInt(svgElement.getAttribute('height') || '0')
+//     };
+//     const dataURL = SVG.toDataURL(svgElement);
+//     expect(dataURL).toBeDefined();
+//     expect(dataURL.startsWith('data:image/svg+xml,')).toBeTruthy();
+
+//     const imageWithSVG = new Image();
+//     imageWithSVG.src = dataURL;
+//     expect(imageWithSVG).toBeDefined();
+//     expect(imageWithSVG.width).toBe(svgDimensions.width);
+//     expect(imageWithSVG.height).toBe(svgDimensions.height);
+//   });
+// });
