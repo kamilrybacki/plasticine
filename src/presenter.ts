b22877155574d80f4303bb3c9ed910e3f2a76501
Kamil Rybacki <kamilandrzejrybacki@gmail.com>
2024-08-07 19:43:11 +0200

    Add snippet test and correct path resolution

--- src/presenter.ts
+++ src/presenter.ts
@@ -1,21 +1,29 @@
 import { EJS } from "@markup/ejs";
 import { CodeSnippet } from "@main/markup/snippet";
 import Transistor from "@renderer/transistor";
+import { fileURLToPath } from 'url';
+import { dirname } from "path";
 
-const DEFAULT_CODE_SNIPPET_LAYOUT = 'templates/code.ejs';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export const DEFAULT_CODE_SNIPPET_LAYOUT = `${__dirname}/templates/code.ejs`;
+export const DEFAULT_CODE_SNIPPET_LANGUAGE = 'plaintext';
 
 export default class Presenter {
   name: string;
+  title: string;
   snapshots: string[] = [];
   readonly layout: string;
   readonly language: string;
 
   constructor(
     name: string,
+    title: string = '',
     layout: string = DEFAULT_CODE_SNIPPET_LAYOUT,
-    language: string = 'plaintext'
+    language: string = DEFAULT_CODE_SNIPPET_LANGUAGE
   ) {
     this.name = name;
+    this.title = title;
     this.layout = layout;
     this.language = language;
   };
@@ -47,6 +55,8 @@ export default class Presenter {
       .render(this.layout, {
         code: new CodeSnippet(frameSource, language).lines,
         name: this.name,
+        titleStyle: 'font-bold text-lg',
+        title: this.title,
       });
   };
 };
--- tests/presenter.test.ts
+++ tests/presenter.test.ts
@@ -1,8 +1,34 @@
 import { describe, it, expect } from '@jest/globals';
-import Presenter from '@main/presenter';
+import Presenter, { DEFAULT_CODE_SNIPPET_LAYOUT, DEFAULT_CODE_SNIPPET_LANGUAGE } from '@main/presenter';
 
 describe('Presenter structure', () => {
     const presenter = new Presenter('John Doe');
+    const HELLO_WORLD_RENDERED = `
+    <main>
+        <pre>
+        <code>
+          Hello, world!
+        </code>
+      </pre>
+    </main>
+    `;
+
     it('should have a name', () => expect(presenter.name).toBe('John Doe'));
-    it('should have code snapshot frames', () => expect(presenter.snapshots).toEqual([]));
+
+    it('should start with empty snapshot frames collection', () => expect(presenter.snapshots).toEqual([]));
+
+    it('should have a default layout', () => expect(presenter.layout).toBe(DEFAULT_CODE_SNIPPET_LAYOUT));
+
+    it('should have a default language', () => expect(presenter.language).toBe(DEFAULT_CODE_SNIPPET_LANGUAGE));
+
+    it('should add a snapshot frame', async () => {
+      await presenter.addSnapshot('Hello, world!')
+        .then(() => {
+          expect(
+            presenter.snapshots.pop()?.replace(/\s/g, '')
+          ).toEqual(
+            HELLO_WORLD_RENDERED.replace(/\s/g, '')
+          );
+        });
+    });
 });
