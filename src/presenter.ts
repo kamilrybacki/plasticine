09c3039af89742b78700ff5e354b4f46b504a79e
Kamil Rybacki <kamilandrzejrybacki@gmail.com>
2024-08-07 13:52:38 +0200

    refactor: Add async/await to Presenter's addFrame method

--- src/presenter.ts
+++ src/presenter.ts
@@ -4,25 +4,26 @@ import { CodeSnippet } from "@main/markup/snippet";
 const DEFAULT_CODE_SNIPPET_LAYOUT = 'templates/code.ejs';
 
 class Presenter {
-  frames: string[];
-  currentFrame: string;
   name: string;
+  frames: string[] = [];
+  readonly layout: string;
 
-  constructor(name: string) {
+  constructor(
+    name: string,
+    layout: string = DEFAULT_CODE_SNIPPET_LAYOUT
+  ) {
     this.name = name;
-    this.frames = [];
-    this.currentFrame = '';
+    this.layout = layout;
   };
 
-  addFrame(source: string, language: string): void {};
+  async addFrame(source: string, language: string): Promise<void> {
+    const renderedFrame = await this.render(source, language);
+    this.frames.push(renderedFrame);
+  };
 
-  async render(
-    layout: string = DEFAULT_CODE_SNIPPET_LAYOUT,
-    source: string,
-    language: string
-  ): Promise<string> {
+  async render(source: string, language: string): Promise<string> {
     return await EJS
-      .render(layout, {
+      .render(this.layout, {
         code: new CodeSnippet(source, language).lines,
         name: this.name,
       });
